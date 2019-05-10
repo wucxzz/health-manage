d4ab2614cc41cd20a259633bca92856c89fc314d
wucxzz <329040688@qq.com>
2019-05-10 23:16:43 +0800

    日常管理页面

--- src/views/daily/Daily.jsx
+++ src/views/daily/Daily.jsx
@@ -1,18 +1,12 @@
 import React from 'react'
 import '../common/common.less'
-import { DatePicker, List, Toast, Picker, Switch  } from 'antd-mobile'
+import { DatePicker, List, Toast, Picker, Switch } from 'antd-mobile'
 import API from '../../components/httpAPI'
+import data from '../common/data'
 
 const nowTimeStamp = Date.now();
 const now = new Date(nowTimeStamp);
-// GMT is not currently observed in the UK. So use UTC now.
-const utcNow = new Date(now.getTime() + (now.getTimezoneOffset() * 60000));
-let minDate = new Date(nowTimeStamp - 1e7);
-const maxDate = new Date(nowTimeStamp + 1e7);
-if (minDate.getDate() !== maxDate.getDate()) {
-  // set the minDate to the 0 of maxDate
-  minDate = new Date(maxDate.getFullYear(), maxDate.getMonth(), maxDate.getDate());
-}
+
 export default class Daily extends React.Component{
   constructor(props) {
     super(props);
@@ -20,6 +14,7 @@ export default class Daily extends React.Component{
       date: now,
       asyncValue: 1,
       checked: true,
+      pickerValue: [],
     };
 
   }
@@ -55,34 +50,6 @@ export default class Daily extends React.Component{
   };
 
   render() {
-    const sleepData = [
-      {label: '非常好', value: 0},
-      {label: '好', value: 1},
-      {label: '一般', value: 2},
-      {label: '差', value: 3},
-      {label: '很差', value: 4}
-    ];
-    const eatData = [
-      {label: '正常', value: 0},
-      {label: '过量', value: 1},
-      {label: '过甜', value: 2},
-      {label: '过咸', value: 3}
-    ];
-    const moodData = [
-      {label: '正常', value: 0},
-      {label: '愉快', value: 1},
-      {label: '伤心', value: 2},
-      {label: '抑郁', value: 3},
-      {label: '紧张', value: 4},
-      {label: '焦虑', value: 5}
-    ];
-    const symptomsData = [
-      {label: '正常', value: 0},
-      {label: '头晕', value: 1},
-      {label: '胸闷', value: 2},
-      {label: '乏力', value: 3},
-      {label: '其他', value: 4}
-    ];
     return (
       <div className="layout">
         <div className="message-input">
@@ -94,7 +61,7 @@ export default class Daily extends React.Component{
             <List.Item arrow="horizontal">时间</List.Item>
           </DatePicker>
           <Picker 
-            data={sleepData} 
+            data={data.sleepData} 
             cols={1} 
             value={this.state.asyncValue}
             onPickerChange={this.onPickerChange}
@@ -113,7 +80,7 @@ export default class Daily extends React.Component{
             />}
           >是否午睡</List.Item>
           <Picker 
-            data={eatData} 
+            data={data.eatData} 
             cols={1} 
             value={this.state.asyncValue}
             onPickerChange={this.onPickerChange}
@@ -132,7 +99,7 @@ export default class Daily extends React.Component{
             />}
           >是否饮酒</List.Item>
           <Picker 
-            data={moodData} 
+            data={data.moodData} 
             cols={1} 
             value={this.state.asyncValue}
             onPickerChange={this.onPickerChange}
@@ -140,19 +107,11 @@ export default class Daily extends React.Component{
           >
             <List.Item arrow="horizontal" onClick={this.onClick}>心情</List.Item>
           </Picker>
-          <Picker 
-            data={symptomsData} 
-            cols={1} 
-            value={this.state.asyncValue}
-            onPickerChange={this.onPickerChange}
-            onOk={v => console.log(v)}
-          >
-            <List.Item arrow="horizontal" onClick={this.onClick}>症状</List.Item>
-          </Picker>
           <div className="button-wrap button-wrap-cover">
             <span className="button-item">取消</span>
             <span>保存</span>
           </div>
+
         </div>
       </div>
     );
